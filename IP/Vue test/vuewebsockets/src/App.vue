3991f3264ab89b4f52e1cde36ff29b250037221d
juju124 <justin555@live.nl>
2022-05-17 11:34:32 +0200

    Added a websocket Test project to try stuff out and the Group project work

---
+++ IP/Vue test/vuewebsockets/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <h2>Vue.js WebSocket Tutorial</h2>
+    <button v-on:click="sendMessage('hello')">Send Message</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: function ()
+  {
+    return {
+      connection: null
+    }
+  },
+  created() {
+    console.log("Starting your new Websocket connection")
+    this.connection = new WebSocket("wss://echo.websocket.org")
+
+    this.connection.onmessage = function (event) {
+      console.log(event)
+    }
+
+    this.connection.onopen = function (event) {
+      console.log(event)
+      console.log("Opened websocket with succes")
+    }
+  },
+  methods: {
+    sendMessage: function(message) {
+      console.log(this.connection)
+      this.connection.send(message)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
